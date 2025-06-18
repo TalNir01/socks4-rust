f242346d6c4af108138e18142df1d33ee6574be8
TalN <taliknir@gmail.com>
2025-06-18 17:38:41 +0300

    WIP: Logs & tracing support

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
+use std::env;
 use std::net::Ipv4Addr;
+use std::process;
+use std::str::FromStr;
 use tokio::io::AsyncWriteExt;
 use tokio::net::{TcpListener, TcpStream};
+use tracing::{debug, error, info, warn};
+use tracing_subscriber;
 
 pub mod socks_constants;
 pub mod socks_specification;
@@ -9,17 +14,36 @@ use crate::socks_specification::{Socks4Request, Socks4Response};
 
 #[tokio::main]
 async fn main() -> tokio::io::Result<()> {
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::DEBUG)
+        .init();
+
+    let cmd_args: Vec<String> = env::args().collect();
+    if cmd_args.len() != 3 {
+        error!("Usage: {} <destination_ip> <destination_port>", cmd_args[0]);
+        process::exit(1); // Exit with an error code
+    }
+
+    let dst_ip = Ipv4Addr::from_str(&cmd_args[1]).unwrap_or_else(|_| {
+        error!("Invalid destination IP address: {}", cmd_args[1]);
+        process::exit(1);
+    });
+    let dst_port: u16 = cmd_args[2].parse().unwrap_or_else(|_| {
+        error!("Invalid destination port: {}", cmd_args[2]);
+        process::exit(1);
+    });
     // TODO: Replace this with command line argument parsing
-    let listener = TcpListener::bind("0.0.0.0:8080").await?;
-    println!("Echo server listening on 0.0.0.0:8080");
+    let bind_address = format!("{}:{}", dst_ip, dst_port);
+    let listener = TcpListener::bind(bind_address.clone()).await?;
+    info!("Socks4 Server Initialized On {bind_address}");
 
     loop {
         let (socket, addr) = listener.accept().await?;
-        println!("Accepted connection from {}", addr);
+        debug!("Accepted connection from {}", addr);
 
         tokio::spawn(async move {
             if let Err(e) = handle_connection(socket).await {
-                eprintln!("Error handling {}: {:?}", addr, e);
+                error!("Error handling {}: {:?}", addr, e);
             }
         });
     }
@@ -34,15 +58,14 @@ async fn handle_connection(mut client: TcpStream) -> tokio::io::Result<()> {
     // Connect to the destination server (hardcoded for now)
     let client_request: Socks4Request =
         Socks4Request::from_stream(&mut client).await.map_err(|e| {
-            eprintln!("Failed to read SOCKS4 request: {}", e);
+            error!("Failed to read SOCKS4 request: {}", e);
             e
         })?;
-    println!("Received SOCKS4 request: {:?}", client_request);
-    println!("{} To {}", client_request.dst_ip, client_request.dst_port);
+    debug!("Received client socks4 request: {:?}", client_request);
 
     match connect_to_destination(client_request.dst_ip, client_request.dst_port).await {
         Ok(mut target_connection) => {
-            println!(
+            info!(
                 "Successfully connected to destination: {}:{}",
                 client_request.dst_ip, client_request.dst_port
             );
@@ -53,28 +76,20 @@ async fn handle_connection(mut client: TcpStream) -> tokio::io::Result<()> {
             };
             // Send success response to the client
             client.write(success_response.to_bytes().as_slice()).await?;
-            println!("Sent success response to client");
+            debug!("Returned success response to client: {success_response:?}");
             tokio::io::copy_bidirectional(&mut client, &mut target_connection).await?; // Propagate errors
             Ok(())
         }
         Err(e) => {
-            eprintln!("Failed to connect to destination: {}", e);
+            error!("Failed to connect to destination: {}", e);
             let failure_response = Socks4Response {
                 status: socks4_status::REQUEST_REJECTED,
                 dst_port: client_request.dst_port,
                 dst_ip: client_request.dst_ip,
             };
             client.write(failure_response.to_bytes().as_slice()).await?;
-            println!("Sent failure response to client: {failure_response:?}");
+            warn!("Returned failure response to client: {failure_response:?}");
             return Err(e);
         }
     }
-
-    // let mut server = TcpStream::connect("127.0.0.1:8088").await?;
-    // println!("Connected to destination server at 127.0.0.1:8080");
-
-    // // Forward: client <=> server (two-way)
-    // tokio::io::copy_bidirectional(&mut client, &mut server).await?; // Propagate errors
-
-    // Ok(())
 }
